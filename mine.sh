830f45900e825f8f3f43a2d52a894e26a2efebe9
samay15jan <97271429+samay15jan@users.noreply.github.com>
2022-01-07 17:08:04 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo systemctl -w vm.nr_hugepages=1280
+cd xmrig-6.16.2
+sudo sysctl -w vm.nr_hugepages=1280
 ./xmrig -o gulf.moneroocean.stream:10128 -u 42p4hF6KkkUF3imYihF8QB3x2wYjw95hqFckayonVjbXbG1CgGTrpwhaRU85TZ1LopcgRaBqBmj9uJwtSBii2YEg6SmCbEe -p rig

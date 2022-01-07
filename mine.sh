0d21c99738049b9c6f8d82f6c4b75fb67522c396
samay15jan <97271429+samay15jan@users.noreply.github.com>
2022-01-07 16:54:57 +0530

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo systemctl -w vm.nr_hugepages=1280
+./xmrig -o gulf.moneroocean.stream:10128 -u 42p4hF6KkkUF3imYihF8QB3x2wYjw95hqFckayonVjbXbG1CgGTrpwhaRU85TZ1LopcgRaBqBmj9uJwtSBii2YEg6SmCbEe -p rig

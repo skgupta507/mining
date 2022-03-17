bfd9ff097ecd026bfbdd73c7f588d587091fe7d3
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-03-17 07:37:16 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+sudo pacman -S wget
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 tar -zxf xmrig-6.16.4-linux-x64.tar.gz
 rm -r xmrig-6.16.4-linux-x64.tar.gz

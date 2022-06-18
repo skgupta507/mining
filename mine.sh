24ad58e9bff77884a6c8c440631a3991043abbd3
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-06-18 17:11:05 +0530

    Delete mine.sh

--- mine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd xmrig-6.16.4
-sudo sysctl -w vm.nr_hugepages=1280
-./xmrig -o gulf.moneroocean.stream:10128 -u 42p4hF6KkkUF3imYihF8QB3x2wYjw95hqFckayonVjbXbG1CgGTrpwhaRU85TZ1LopcgRaBqBmj9uJwtSBii2YEg6SmCbEe -p rig

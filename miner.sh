1b4d7e2050c5f0d224041ac6d1a47ff1b80ece47
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-06-18 17:10:39 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -3,9 +3,9 @@ sudo pacman -S wget --noconfirm
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -zxf xmrig-6.17.0-linux-x64.tar.gz
 rm -r xmrig-6.17.0-linux-x64.tar.gz
+echo "#!/bin/bash
 cd xmrig-6.17.0
-echo 
-"sudo sysctl -w vm.nr_hugepages=1280 
+sudo sysctl -w vm.nr_hugepages=1280 
 ./xmrig -o gulf.moneroocean.stream:10128 -u 42p4hF6KkkUF3imYihF8QB3x2wYjw95hqFckayonVjbXbG1CgGTrpwhaRU85TZ1LopcgRaBqBmj9uJwtSBii2YEg6SmCbEe" >> mine.sh 
 chmod u+x mine.sh
 rm -r miner.sh

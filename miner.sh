4d8e50b3afa2df7db7fcb19a938c49f840efaf33
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-06-18 16:59:28 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -S wget --noconfirm
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar -zxf xmrig-6.16.4-linux-x64.tar.gz
-rm -r xmrig-6.16.4-linux-x64.tar.gz
+tar -zxf xmrig-6.17.0-linux-x64.tar.gz
+rm -r xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.16.4 
 echo 
 "sudo sysctl -w vm.nr_hugepages=1280 
